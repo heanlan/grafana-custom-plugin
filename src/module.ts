23bc6482ad3296551950f0e17922590f8a771195
heanlan <hanlan@vmware.com>
2022-01-09 16:24:19 -0500

    fix lint errors

--- src/module.ts
+++ src/module.ts
@@ -2,4 +2,4 @@ import { PanelPlugin } from '@grafana/data';
 import { SankeyOptions } from './types';
 import { SankeyPanel } from './SankeyPanel';
 
-export const plugin = new PanelPlugin<SankeyOptions>(SankeyPanel).setPanelOptions(builder => builder);
+export const plugin = new PanelPlugin<SankeyOptions>(SankeyPanel).setPanelOptions((builder) => builder);
